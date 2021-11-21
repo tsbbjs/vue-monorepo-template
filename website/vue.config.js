c3f566471051f7ef4e1b30001828becca32a8e2a
jaywcjlove <398188662@qq.com>
2021-11-21 23:08:12 +0800

    chore: update .github/workflows/ci.yml

--- website/vue.config.js
+++ website/vue.config.js
@@ -6,6 +6,7 @@ module.exports = {
       symlinks: false,
       alias: {
         vue: path.resolve(`./node_modules/vue`),
+        'vue-router': path.resolve(`./node_modules/vue-router`),
       },
     },
   },
