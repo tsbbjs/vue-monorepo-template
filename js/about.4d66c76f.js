341f3ace1dd3356c56aafd24855920e407c1d418
jaywcjlove <jaywcjlove@users.noreply.github.com>
2021-11-23 05:01:49 +0000

    deploy: dc32294f0b9f93c5d33a5d50ff5ef7ea8b6bc3cb

--- js/about.4d66c76f.js
+++ js/about.4d66c76f.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var c=a("7a23"),e={class:"about"},o=Object(c["d"])("h1",null,"This is an about page",-1),s=[o];function u(n,t){return Object(c["o"])(),Object(c["c"])("div",e,s)}var b=a("6b0d"),i=a.n(b);const r={},d=i()(r,[["render",u]]);t["default"]=d}}]);
-//# sourceMappingURL=about.ed14ea62.js.map
+//# sourceMappingURL=about.4d66c76f.js.map
